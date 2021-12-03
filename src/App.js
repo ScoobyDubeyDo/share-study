c93350c8548c9f546cad458abff462dccb794125
aman-can <aman.dubey1998@gmail.com>
2021-12-04 02:46:14 +0530

    made login - logup pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
     return (
         <div className="App">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum,
-            illum libero nulla, deleniti sit molestias fugit cum voluptatibus
-            amet cupiditate dolor minus eos.
+            <Router>
+                <Switch>
+                    {/* <Route exact path="/" component={Homepage} /> */}
+                    <ProtectedRoute exact path="/signin" component={SignIn} />
+                    <ProtectedRoute exact path="/signup" component={SignUp} />
+                    <ProtectedRoute
+                        exact
+                        path="/dashboard"
+                        component={Dashboard}
+                    />
+                    {/* <Route
+                        exact
+                        path="/protected-page"
+                        component={ProtectedPage}
+                    />
+                    <Route
+                        exact
+                        path="/forgot-password"
+                        component={ForgotPasswordPage}
+                    />
+                    <Route
+                        exact
+                        path="/reset-password"
+                        component={ResetPasswordPage}
+                    />
+                    <Route exact path="*" component={NotfoundPage} /> */}
+                </Switch>
+            </Router>
         </div>
     );
 }
 
+// function ProtectedRoute(props) {}
+
 export default App;
-  
