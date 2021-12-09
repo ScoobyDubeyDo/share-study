d5f7eb9cfb699929d787983065150639ceb46dd7
aman-can <aman.dubey1998@gmail.com>
2021-12-09 14:07:49 +0530

    updated notes Student

--- src/pages/NotesStudent.js
+++ src/pages/NotesStudent.js
@@ -16,22 +16,27 @@ import {
     CircularProgress,
 } from "@mui/material";
 import { Masonry } from "@mui/lab";
+import { SentimentVeryDissatisfiedTwoTone } from "@mui/icons-material";
 function NotesStudent() {
     const [backdropOpen, setBackdropOpen] = useState(false);
     const noteRef = collection(db, "notes");
     const [notesData, setNotesData] = useState([]);
+    const [noteIDs, setNoteIDs] = useState([]);
 
     useEffect(() => {
         let assr = [];
+        let noteKey = [];
         setBackdropOpen(true);
         getDocs(noteRef)
             .then((data) => {
                 data.forEach((doc) => {
                     assr.push(doc.data());
+                    noteKey.push(doc.id);
                 });
             })
             .then(() => {
                 setNotesData([...assr]);
+                setNoteIDs([...noteKey]);
                 setBackdropOpen(false);
             });
     }, []);
@@ -47,96 +52,64 @@ function NotesStudent() {
             >
                 <CircularProgress color="inherit" />
             </Backdrop>
-            <Box maxWidth="md" sx={{ m: "auto" }}>
-                <Masonry columns={{ xs: 1, md: 4, sm: 2 }}>
-                    {notesData.map((doc) => {
-                        return (
-                            <Card>
-                                <CardContent>
-                                    <Typography
-                                        variant="h5"
-                                        color="text.secondary"
-                                        gutterBottom
-                                    >
-                                        {doc.noteTitle}
-                                    </Typography>
-                                    <Typography
-                                        // sx={{ display: "inline-block" }}
-                                        variant="body2"
-                                        gutterBottom
-                                        color="text.secondary"
-                                    >
-                                        {`${doc.teacherName} • ${doc.subject}`}
-                                    </Typography>
-                                    <Typography variant="body1">
-                                        Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit. Quos culpa at eius
-                                        delectus neque perspiciatis illum ad
-                                        ipsraesentium voluptate nulla, sit
-                                        delectus fugit aut, tempore illo!
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button
-                                        component="a"
-                                        size="small"
-                                        href={doc.noteURL}
-                                    >
-                                        Open note
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        );
-                    })}
-                </Masonry>
+            <Box maxWidth="md" sx={{ mx: "auto", my: 2 }}>
+                {notesData ? (
+                    <Masonry columns={{ xs: 1, md: 4, sm: 2 }} sx={{ m: 0 }}>
+                        {notesData.map((doc, index) => {
+                            return (
+                                <Card key={noteIDs[index]}>
+                                    <CardContent>
+                                        <Typography
+                                            variant="h5"
+                                            color="text.secondary"
+                                            gutterBottom
+                                        >
+                                            {doc.noteTitle}
+                                        </Typography>
+                                        <Typography
+                                            // sx={{ display: "inline-block" }}
+                                            variant="body2"
+                                            gutterBottom
+                                            color="text.secondary"
+                                        >
+                                            {`${doc.teacherName} • ${doc.subject}`}
+                                        </Typography>
+                                        <Typography variant="body1">
+                                            Lorem ipsum dolor sit amet
+                                            consectetur adipisicing elit. Quos
+                                            culpa at eius delectus neque
+                                            perspiciatis illum ad ipsraesentium
+                                            voluptate nulla, sit delectus fugit
+                                            aut, tempore illo!
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button
+                                            component="a"
+                                            size="small"
+                                            href={doc.noteURL}
+                                        >
+                                            Open note
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            );
+                        })}
+                    </Masonry>
+                ) : (
+                    <Card>
+                        <CardContent>
+                            <Button
+                                disabled
+                                fullWidth
+                                endIcon={<SentimentVeryDissatisfiedTwoTone />}
+                            >
+                                No notes
+                            </Button>
+                        </CardContent>
+                    </Card>
+                )}
             </Box>
-            {/* <Masonry>
-                {notesData.map((doc) => {
-                    return (
-                        <Card sx={{ minWidth: 275 }}>
-                            <CardContent>
-                                <Typography
-                                    sx={{ fontSize: 20, fontWeight: 600 }}
-                                    color="text.secondary"
-                                    gutterBottom
-                                >
-                                    {doc.noteTitle}
-                                </Typography>
-
-                                <Typography
-                                    sx={{ display: "inline-block" }}
-                                    gutterBottom
-                                    color="text.secondary"
-                                >
-                                    {doc.teacherName}
-                                </Typography>
-                                <Typography
-                                    sx={{ display: "inline-block", mx: 2 }}
-                                    gutterBottom
-                                    color="text.secondary"
-                                >
-                                    •
-                                </Typography>
-                                <Typography
-                                    sx={{ display: "inline-block" }}
-                                    gutterBottom
-                                    color="text.secondary"
-                                >
-                                    {doc.subject}
-                                </Typography>
-                                <Typography variant="body2">
-                                    notes desp..
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <a size="small" href={doc.noteURL}>
-                                    Open note
-                                </a>
-                            </CardActions>
-                        </Card>
-                    );
-                })}
-            </Masonry> */}
         </Container>
     );
 }
